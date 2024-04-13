61a6cfaa452363e36f14dfdb782511ed5163ccb6
Tim Schmidt <tim@patternfi.com>
2024-04-13 00:28:39 -0500

    create app

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p className="flex w-full justify-center text-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        Hello World!<br />
+        I&apos;m Meg Shoemaker<br />
+        and I&apos;m a designer
+      </p>
+    </main>
+  );
+}
