5b2a6e10f94c756c617afb4391b3f6614c88dd68
Tim Schmidt <tim@patternfi.com>
2024-05-08 00:25:46 -0500

    more sections

--- app/projects/trapped-downtown/page.tsx
+++ app/projects/trapped-downtown/page.tsx
@@ -23,7 +23,22 @@ export default function TrappedDowntown() {
         <Section
           title="Research"
           header="What are people seeing when they book an escape room?"
-          content="I needed to conduct user research to really understand the needs of their current and potential customers. To get up to speed on current trends and relevant content, I did a competitive analysis on four Escape Room businesses located across the country."
+          content={[
+            "I needed to conduct user research to really understand the needs of their current and potential customers. To get up to speed on current trends and relevant content, I did a competitive analysis on four Escape Room businesses located across the country.",
+          ]}
+        />
+        <Section
+          header="Evaluating the current experience"
+          content={[
+            "The Trapped Downtown website was initially built 7 years ago. There was an opportunity to audit the website against UX best practices and accessibility considerations.",
+          ]}
+        />
+        <Section
+          header="What do people think, feel, and do when booking events in advance?"
+          content={[
+            "I interviewed 8 users about their experience researching, selecting, and booking events in advance (e.g. axe throwing, cooking classes). I also asked specific question about their experience with escape rooms, as this was of interest to our stakeholders.",
+            "Following the interviews, I reviewed the notes and created an Empathy Map to better understand the perspective of our users.",
+          ]}
         />
       </div>
     </main>
--- components/Section/index.tsx
+++ components/Section/index.tsx
@@ -3,18 +3,22 @@ import React from "react";
 interface SectionProps {
   title?: string;
   header: string;
-  content: string;
+  content: string[];
 }
 
 const Section = ({ title, header, content }: SectionProps) => {
   return (
     <section className="flex flex-col px-10 py-20 md:flex-row md:px-40 md:py-30">
-      <div className={`w-96 ${title ? "mb-10" : ""}`}>
+      <div className={`md:min-w-32 lg:min-w-56 ${title ? "mb-10" : ""}`}>
         <h5>{title?.toUpperCase()}</h5>
       </div>
       <div>
         <h4 className="mb-4">{header}</h4>
-        <p>{content}</p>
+        {content.map((paragraph, index) => (
+          <p key={index} className="mb-4">
+            {paragraph}
+          </p>
+        ))}
       </div>
     </section>
   );
