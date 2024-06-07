e474c6b0363fe32cb6f6e2750206f3c5f939254d
Tim Schmidt <tim@patternfi.com>
2024-06-06 22:47:31 -0500

    about me

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,27 +3,42 @@ import headshot from "../../public/Shoemaker_Meg-28544.jpg";
 
 export default function About() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center w-full md:flex-row-reverse md:justify-end md:w-1/2">
-        {/* Text Container */}
-        <div className="md:ml-8">
-          <h3 className="text-3xl font-semibold text-left dark:text-white">
-            Welcome!
-          </h3>
-          <p className="text-left mt-2 dark:text-gray-300">
-            Here's a little bit about me.
-          </p>
-        </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-12 sm:p-24">
+      <div className="flex flex-col items-start w-full md:flex-row md:justify-end md:w-10/12">
         {/* Image */}
-        <div className="mt-4 md:mt-0">
+        <div className="mt-4 md:mt-0 md:mx-8">
           <Image
             src={headshot}
             alt="Meg Shoemaker headshot"
-            className="rounded-lg"
-            width={200}
-            height={200}
+            className="rounded-lg mb-12"
             placeholder="blur"
           />
+        </div>
+        {/* Text Container */}
+        <div className="md:mx-8">
+          <div>
+            <p className="text-lg text-left mb-6">About Meg</p>
+            <p className="text-3xl text-left mt-2 my-6">I'm a UX Designer.</p>
+            <p className="text-3xl text-left mt-2 my-6">
+              My greatest strength is my empathy and I have a talent for
+              fostering collaboration and focusing on the details.
+            </p>
+            <p className="text-3xl text-left mt-2 my-6">
+              I value harmony and bringing curiosity to every conversation.
+            </p>
+          </div>
+          <div>
+            <p className="text-lg text-left mb-6 mt-12">{`HR ⇢ UX`}</p>
+            <p className="text-3xl text-left mt-2 my-6">
+              In my previous career as an HR Business Partner, I partnered with
+              Technology and Design teams for 8+ years. I empathized with
+              employee's needs and built seamless employee experiences.
+            </p>
+            <p className="text-3xl text-left mt-2 my-6">
+              I'm ready to bring my collaborative spirit to the iterative and
+              creative process of UX Design.
+            </p>
+          </div>
         </div>
       </div>
     </main>
