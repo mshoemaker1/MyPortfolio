0d94382ffacadb8f6d405223b2c018d7b8781c5f
Tim Schmidt <tim@patternfi.com>
2024-06-24 19:52:04 -0500

    little cup icon spacing

--- app/projects/little-cup/ResearchPins.tsx
+++ app/projects/little-cup/ResearchPins.tsx
@@ -6,29 +6,30 @@ export const ResearchPins = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center">
       <div className="flex justify-center pt-10 w-full max-w-[400px] h-[470px] mx-8 rounded-lg border-lavender-500 border-[5px] mb-16">
-        <div className="flex flex-col w-[255px] h-[340px] justify-between items-center">
+        <div className="flex flex-col w-[255px] h-[340px] items-center">
           <Image src={pin002} alt="Map pin" />
-          <div>
+          <div className="mt-8">
             <p className="text-[15px]">PAIN POINT</p>
             <p>
               Maps are cluttered with businesses, making it an overwhelming user
               experience.
             </p>
           </div>
-          <div>
+          <div className="mt-8">
             <p className="text-[15px]">OPPORTUNITY</p>
             <p>The design of the map will need to be clear and compelling.</p>
           </div>
         </div>
       </div>
       <div className="flex justify-center pt-10 w-full max-w-[400px] h-[470px] mx-8 rounded-lg border-lavender-500 border-[5px]">
-        <div className="flex flex-col w-[255px] h-[340px] justify-between items-center">
-          <Image src={pin001} alt="Map pin" />
-          <div>
+        <div className="flex flex-col w-[255px] h-[340px] items-center">
+          <Image src={pin001} alt="Map pin" height={83} />
+          <div className="mt-8">
             <p className="text-[15px]">PAIN POINT</p>
             <p>Users want to be able to filter for specific things.</p>
+            <br />
           </div>
-          <div>
+          <div className="mt-8">
             <p className="text-[15px]">OPPORTUNITY</p>
             <p>
               Tailored filters will be an important differentiator to attract
